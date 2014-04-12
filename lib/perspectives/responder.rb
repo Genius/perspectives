b06c6f0378b6415f72b470ce932f0503c6841759
Andrew Warner <wwarner.andrew@gmail.com>
2014-04-12 16:25:51 -0400

    Use perspectives_enabled_action? instead of type check

--- lib/perspectives/responder.rb
+++ lib/perspectives/responder.rb
@@ -1,13 +1,13 @@
 module Perspectives
   class Responder < ActionController::Responder
     def to_html
-      return super unless resource.is_a?(Perspectives::Base)
+      return super unless controller.__send__(:perspectives_enabled_action?)
 
       render text: resource.to_html, layout: :default
     end
 
     def to_json
-      return super unless resource.is_a?(Perspectives::Base)
+      return super unless controller.__send__(:perspectives_enabled_action?)
 
       render json: resource
     end
