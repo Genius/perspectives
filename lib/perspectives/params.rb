38f96f3be701297a137e078aab8d1b8b3817d54d
Andrew Warner <wwarner.andrew@gmail.com>
2014-04-12 22:29:34 -0400

    Enforce exact params list

--- lib/perspectives/params.rb
+++ lib/perspectives/params.rb
@@ -4,8 +4,9 @@ module Perspectives
       base.class_eval do
         extend ClassMethods
 
-        config_attribute :_required_params
+        config_attribute :_required_params, :_optional_params
         self._required_params = []
+        self._optional_params = []
         attr_reader :_params, :context
       end
     end
@@ -21,9 +22,12 @@ module Perspectives
 
     def assert_valid_params!
       missing = _required_params.select { |l| !_params.key?(l) }
+      unknown = _params.keys - (_required_params + _optional_params)
 
       if missing.any?
         raise ArgumentError, "Missing #{missing.join(', ').inspect} while initializing #{self.class}!"
+      elsif unknown.any?
+        raise ArgumentError, "Unrecognized params #{unknown.join(', ').inspect} while initializing #{self.class}!"
       end
     end
 
@@ -31,7 +35,12 @@ module Perspectives
       def param(*param_names)
         options = param_names.extract_options!
 
-        self._required_params += param_names unless options[:allow_nil]
+        if options[:allow_nil]
+          self._optional_params += param_names
+        else
+          self._required_params += param_names
+        end
+
         param_names.each { |n| define_method(n) { _params[n] } }
       end
     end
